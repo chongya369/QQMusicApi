5552b41b48d482fa2391a913767aacddef103707
chongya369 <chongya369@163.com>
2026-09-13 12:25:08 +0800

    chore: 版本号更新为 0.7.2-dev.1

--- qqmusic_api/__init__.py
+++ qqmusic_api/__init__.py
@@ -22,7 +22,7 @@ from .core.exceptions import (
 from .core.versioning import Platform
 from .models.request import Credential
 
-__version__ = "0.7.2"
+__version__ = "0.7.2-dev.1"
 
 __all__ = [
     "ApiDataError",
